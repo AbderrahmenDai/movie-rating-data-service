a697cc06349f204e9e01f768a20009b737c908e1
Abderrahmen dai <abderrahmen.dai.11@gmail.com>
2023-04-11 23:48:21 +0100

    Initial commit

---
+++ src/main/java/io/java/movieratingdataservice/models/UserRating.java
@@ -0,0 +1,19 @@
+package io.java.movieratingdataservice.models;
+
+import java.util.List;
+
+public class UserRating {
+
+    private List<Rating> userRating;
+
+    public List<Rating> getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(List<Rating> userRating) {
+        this.userRating = userRating;
+    }
+
+    public void getUserRating(List<Rating> ratings) {
+    }
+}
